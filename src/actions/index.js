fb91af6846d3c126abb2e82fbb4396f9861e3441
LauzarateARG <lautarozarate212001@gmail.com>
2021-12-17 01:01:32 -0300

    https

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export function getCurrentCity(city) {
   return async function (dispath) {
     let apikey = process.env.REACT_APP_API_KEY;
     var json = await axios(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
     );
     try {
       return dispath({
